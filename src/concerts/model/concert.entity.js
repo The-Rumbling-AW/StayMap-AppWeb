83fb58fcfae8d14fb4aa6d8448a25f6d1e8272ca
aria <168163983+ariaalizz@users.noreply.github.com>
2025-05-16 17:47:19 -0500

    Update concert.entity.js

--- src/concerts/model/concert.entity.js
+++ src/concerts/model/concert.entity.js
@@ -1,24 +1,15 @@
 export class Concert {
-    constructor({
-                    id = '',
-                    artist = [],
-                    date = '',
-                    venue = {},
-                    description = '',
-                    genre = '',
-                    image = '',
-                    status = ''
-                }) {
-        this.id = id;
-        this.artistName = artist.length > 0 ? artist[0].name : '';
-        this.date = date;
-        this.venueName = venue.name || '';
-        this.venueLat = venue.location?.lat || 0;
-        this.venueLng = venue.location?.lng || 0;
-        this.venueAddress = venue.address || '';
-        this.description = description;
-        this.genre = artist.length > 0 ? artist[0].genre : genre;
-        this.image = image;
-        this.status=status;
-    }
+  constructor(data = {}) {
+    this.id = data.id;
+    this.artist = data.artist || [];
+    this.artistName = data.artist?.[0]?.name || "Sin nombre";
+    this.genre = data.artist?.[0]?.genre || "Desconocido";
+    this.image = data.image;
+    this.date = data.date;
+    this.status = data.status;
+    this.description = data.description;
+    this.venue = data.venue || {};
+    this.venueName = data.venue?.name || "Sin venue";
+    this.venueAddress = data.venue?.address || "";
+  }
 }
